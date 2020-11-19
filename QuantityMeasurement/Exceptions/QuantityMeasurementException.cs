dc7ccee259ae900f2a306f4f4508571785eb65c6
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-19 20:25:42 +0530

    [Kuna] ADD: Exceptions for Quantity measusrement

---
+++ QuantityMeasurement/Exceptions/QuantityMeasurementException.cs
@@ -0,0 +1,16 @@
+﻿using QuantityMeasurement.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuantityMeasurement.Exceptions
+{
+    class QuantityMeasurementException : Exception
+    {
+        EXCEPTIONENUMS exceptionType;
+        public QuantityMeasurementException(EXCEPTIONENUMS exceptionType) : base(exceptionType.ToString())
+        {
+            this.exceptionType = exceptionType;
+        }
+    }
+}
