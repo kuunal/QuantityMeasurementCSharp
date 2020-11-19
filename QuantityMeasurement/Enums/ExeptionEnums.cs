323081b90d21093b146ba041ebfd8446c1947ca2
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-19 20:18:33 +0530

    [Kunal] ADD: Enums for units

---
+++ QuantityMeasurement/Enums/ExeptionEnums.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuantityMeasurement.Enums
+{
+    public enum EXCEPTIONENUMS
+    {
+        QUANTITY_MISMATCH
+    }
+}
---
+++ QuantityMeasurement/Enums/Units.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace QuantityMeasurement.Enums
+{
+    public enum Units
+    {
+        [Description("Length")]
+        FEET = 12,
+
+        [Description("Length")]
+        INCH = 1
+    }
+}
