9c3c1a03e951d8fec9e83fac255de75ee7c2ed69
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-19 20:26:56 +0530

    [Kunal] ADD: Method for comparing two units

---
+++ QuantityMeasurement/QuantityMeasurements.cs
@@ -0,0 +1,40 @@
+﻿using QuantityMeasurement.Enums;
+using QuantityMeasurement.Exceptions;
+using System;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace QuantityMeasurement
+{
+    public class QuantityMeasurements
+    {
+
+        double quantity;
+        string unit; 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public QuantityMeasurements(double quantity, Units unit )
+        {
+            this.quantity = quantity;
+            this.unit = GetDescription(unit);
+        } 
+
+
+        public bool CheckEquals(QuantityMeasurements that)
+        {
+            if (this.unit != that.unit)
+                throw new QuantityMeasurementException(EXCEPTIONENUMS.QUANTITY_MISMATCH);
+            return (this.quantity == that.quantity);
+        }
+
+        public string GetDescription(Units unit)
+        {
+            var field = unit.GetType().GetField(unit.ToString());
+            var attr = field.GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return (attr[0] as DescriptionAttribute).Description;
+        }
+    }
+}
